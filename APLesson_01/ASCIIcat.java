4c2be27317a7930785efa7ad4d46fbd162fb545f
anademenko <anademenko@outlook.com>
2016-09-02 13:26:16 -0700

    Added Files

---
+++ APLesson_01/ASCIIcat.java
@@ -0,0 +1,11 @@
+public class ASCIIcat
+{
+	public static void main (String[]args)
+	{
+		System.out.println("   /\\_/\\  (");
+		System.out.println(" ( ^.^ ) _)");
+		System.out.println("    \\ \" /  (");
+		System.out.println("  (  | |  )");
+		System.out.println("(__d b__)");
+	}
+}
